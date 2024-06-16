48c49f05d82e101fa554046fe9d2ee72b307d1ec
pandeyashishkp <pandey.ashishkp@gmail.com>
2024-06-16 12:23:59 +0530

    my new build

--- bucket_clean.py
+++ bucket_clean.py
@@ -1,8 +1,5 @@
 from google.cloud import storage
 import pathlib
-from flask import Flask
-import os
-
 
 app = Flask(__name__)
 
@@ -15,11 +12,14 @@ class GCSBucketDeletion:
         self.project        = kwargs['project']
         self.gcspath        = pathlib.Path(kwargs['gcspath']).parts[0]
         self.remaining_path = str(pathlib.Path(kwargs['gcspath']).parts[1:])
-        self.path           = f"{self.remaining_path}/workflows/"
         self.client         = storage.Client(project=self.project)
         self.bucket         = self.client.bucket(self.gcspath)
     
     def _gcs_deletion(self):
+        if self.remaining_path == '.':
+            self.path = "workflows/"
+        else:
+            self.path = f"{self.remaining_path}/workflows"
         blobs = self.bucket.list_blobs(prefix=self.path)
         for blob in blobs:
             blob.delete()
@@ -27,5 +27,3 @@ class GCSBucketDeletion:
 
 if __name__ == '__main__':
     gcs = GCSBucketDeletion(project='ash11123', gcspath='ash11123')
-    port = int(os.environ.get('PORT', 8080))
-    app.run(debug=True, host='0.0.0.0', port=port)
